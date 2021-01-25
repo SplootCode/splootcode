2bfdf38ae13a0a0a536617e7c1ddbd5ce29a5c3c
Martin Kealey <from-github@kurahaupo.gen.nz>
2021-01-25 13:49:14 +1000

    Update dummy.ts (#2)
    typosquashing

--- scripts/dummy.ts
+++ scripts/dummy.ts
@@ -1,4 +1,4 @@
-// These are here to be explicit references to build in type definitions
+// These are here to be explicit references to the built-in type definitions
 // I couldn't find a way to get from a type keyword like 'string' to the actual 'String' definition.
 // TBH this is sort of ridiculous.
 let a : String;
