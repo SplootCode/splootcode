ba9557cde16e61cb94cae76ca62e6f29761fe2a3
Katie Bell <katie@katharos.id.au>
2021-03-29 13:30:24 +1000

    Add hover style block

--- src/language/type_loader.ts
+++ src/language/type_loader.ts
@@ -45,6 +45,7 @@ import { JssStyleBlock } from "./types/jss_styles/jss_style_block";
 import { ImportDefaultStatement } from "./types/js/import_default";
 import { JssClassBlock } from "./types/jss_styles/jss_class_block";
 import { JssStyleProperty } from "./types/jss_styles/jss_style_property";
+import { JssHoverBlock } from "./types/jss_styles/jss_hover_block";
 
 
 export function loadTypes() {
@@ -96,6 +97,7 @@ export function loadTypes() {
   JssStyleBlock.register();
   JssClassBlock.register();
   JssStyleProperty.register();
+  JssHoverBlock.register();
 
   JavascriptFile.register();
 
