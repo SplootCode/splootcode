0fd1984e2cad848031e6685fd5a66ea759bf0d23
François Wouts <f@zenc.io>
2021-07-17 09:03:37 +1000

    Extract theming into a separate module to enable React Preview support

---
+++ src/providers.tsx
@@ -0,0 +1,26 @@
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import React from "react";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const styles = {
+  global: (props) => ({
+    body: {
+      // Override the chakra default dark mode background color to match editor.css
+      bg: mode("whiteAlpha.900", "#13171b")(props),
+    },
+  }),
+};
+
+const theme = extendTheme({ config, styles });
+
+export const AppProviders: React.FC = ({ children }) => (
+  <ChakraProvider theme={theme} resetCSS={true}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    {children}
+  </ChakraProvider>
+);
