319356d3aa3423610c98fdf12c909da5d6a15dde
Katie Bell <katie@katharos.id.au>
2021-09-09 11:10:21 +1000

    Fix bug where the text widths are wrong on first render due to font loading.

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "tslib";
 import { App } from "./app";
+import { stringWidth } from "./layout/rendered_childset_block";
 import { AppProviders } from "./providers";
 
 const root = document.getElementById("app-root");
+
+// Force the web font to be loaded as soon as the page loads (before we try to render the editor).
+stringWidth('loadfontplz')
 
 ReactDOM.render(
   <AppProviders>
