a029323450f0bbef984731c0fc118c064dd97cb8
Katie Bell <katie@katharos.id.au>
2021-12-17 18:13:32 +1100

    Go back to webpack-dev-server instead of firebase

--- frameview.webpack.dev.ts
+++ frameview.webpack.dev.ts
@@ -1,10 +1,25 @@
 import Dotenv from 'dotenv-webpack'
 import common from './frameview.webpack.common'
 import { Configuration } from 'webpack'
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 import { merge } from 'webpack-merge'
 
-module.exports = merge<Configuration>(common as Configuration, {
+interface Config extends Configuration {
+  devServer?: DevServerConfiguration
+}
+
+module.exports = merge<Config>(common as Configuration, {
   mode: 'development',
+
+  devServer: {
+    port: 3001,
+    hot: false,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Resource-Policy': 'same-site',
+    },
+  },
 
   plugins: [new Dotenv({ safe: true, path: 'development.env' })],
 })
--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,10 +1,25 @@
 import Dotenv from 'dotenv-webpack'
 import common from './webpack.common'
 import { Configuration } from 'webpack'
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 import { merge } from 'webpack-merge'
 
-module.exports = merge<Configuration>(common as Configuration, {
+interface Config extends Configuration {
+  devServer?: DevServerConfiguration
+}
+
+module.exports = merge<Config>(common as Configuration, {
   mode: 'development',
+
+  devServer: {
+    port: 3000,
+    hot: true,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Resource-Policy': 'same-origin',
+    },
+  },
 
   plugins: [new Dotenv({ safe: true, path: 'development.env' })],
 })
