504d5546a844eb22fdfed24148f1c92488ad0d09
Katie Bell <katie@katharos.id.au>
2022-02-23 19:19:39 +1100

    Rearrange the tray (again)

--- packages/editor/components/tray/scope_tray.tsx
+++ packages/editor/components/tray/scope_tray.tsx
@@ -1,17 +1,60 @@
 import React, { useEffect, useState } from 'react'
 
-import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Text } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Text } from '@chakra-ui/react'
+import { Category, MicroNode, getNodeBlock } from './category'
 import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import { MicroNode, getNodeBlock } from './category'
 import { NodeBlock } from '../../layout/rendered_node'
 import { Scope } from '@splootcode/core/language/scope/scope'
 import { ScopeObserver } from '@splootcode/core/language/observers'
 import { SplootNode } from '@splootcode/core/language/node'
+import { TrayCategory } from '@splootcode/core/language/tray/tray'
 import { globalMutationDispatcher } from '@splootcode/core/language/mutations/mutation_dispatcher'
 
 export interface EntryProps {
   rootNode: SplootNode
   startDrag: (node: NodeBlock, offsetX: number, offsetY: number) => any
+}
+
+const AssignmentCategory: TrayCategory = {
+  category: 'Assignment',
+  entries: [
+    {
+      key: 'assign',
+      abstract: 'Creates new variables with the given name and value.',
+      serializedNode: {
+        type: 'PYTHON_ASSIGNMENT',
+        childSets: { left: [], right: [{ type: 'PYTHON_EXPRESSION', childSets: { tokens: [] }, properties: {} }] },
+        properties: {},
+      },
+      examples: [
+        {
+          serializedNode: {
+            type: 'PYTHON_ASSIGNMENT',
+            childSets: {
+              left: [{ type: 'PY_IDENTIFIER', properties: { identifier: 'num' }, childSets: {} }],
+              right: [
+                {
+                  type: 'PYTHON_EXPRESSION',
+                  childSets: {
+                    tokens: [
+                      {
+                        type: 'NUMERIC_LITERAL',
+                        properties: { value: '10' },
+                        childSets: {},
+                      },
+                    ],
+                  },
+                  properties: {},
+                },
+              ],
+            },
+            properties: {},
+          },
+          description: '',
+        },
+      ],
+    },
+  ],
 }
 
 export const ScopeTray = (props: EntryProps) => {
@@ -41,8 +84,16 @@ export const ScopeTray = (props: EntryProps) => {
               {isExpanded ? <ChevronDownIcon mr={1} /> : <ChevronRightIcon mr={1} />}
               <Text>Variables</Text>
             </AccordionButton>
-            <AccordionPanel pl={1} mb={2}>
-              <ScopeTree scope={rootScope} startDrag={startDrag} />
+            <AccordionPanel py={0} pl={2} pr={0}>
+              {rootScope.hasEntries() ? (
+                <Box borderY={'solid 1px'} borderColor={'gray.600'} py={1}>
+                  <Text textColor={'gray.400'} lineHeight={1.1} py={2} px={1}>
+                    Global
+                  </Text>
+                  <ScopeTree scope={rootScope} startDrag={startDrag} />
+                </Box>
+              ) : null}
+              <Category category={AssignmentCategory} startDrag={startDrag} />
             </AccordionPanel>
           </>
         )}
