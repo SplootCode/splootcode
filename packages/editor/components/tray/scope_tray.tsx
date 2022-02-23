7f4b91d6fae9cf600cd9185d527a63104884b925
Katie Bell <katie@katharos.id.au>
2022-02-23 18:58:47 +1100

    Rearrange the tray  bit

--- packages/editor/components/tray/scope_tray.tsx
+++ packages/editor/components/tray/scope_tray.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Text } from '@chakra-ui/react'
+import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import { MicroNode, getNodeBlock } from './category'
 import { NodeBlock } from '../../layout/rendered_node'
 import { Scope } from '@splootcode/core/language/scope/scope'
 import { ScopeObserver } from '@splootcode/core/language/observers'
 import { SplootNode } from '@splootcode/core/language/node'
-import { Text } from '@chakra-ui/react'
 import { globalMutationDispatcher } from '@splootcode/core/language/mutations/mutation_dispatcher'
 
 export interface EntryProps {
@@ -32,12 +33,21 @@ export const ScopeTray = (props: EntryProps) => {
   }, [rootNode])
 
   return (
-    <>
-      <Text textColor={'gray.400'} lineHeight={1.1} py={2} px={1}>
-        Module Scope
-      </Text>
-      <ScopeTree scope={rootScope} startDrag={startDrag} />
-    </>
+    <Accordion allowMultiple defaultIndex={[0]}>
+      <AccordionItem border={'none'}>
+        {({ isExpanded }) => (
+          <>
+            <AccordionButton size={'sm'} border={'none'} px={0} py={1}>
+              {isExpanded ? <ChevronDownIcon mr={1} /> : <ChevronRightIcon mr={1} />}
+              <Text>Variables</Text>
+            </AccordionButton>
+            <AccordionPanel pl={1} mb={2}>
+              <ScopeTree scope={rootScope} startDrag={startDrag} />
+            </AccordionPanel>
+          </>
+        )}
+      </AccordionItem>
+    </Accordion>
   )
 }
 
