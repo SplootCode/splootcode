948a31fc8691b5075a5fd970e9bcd024e658d629
Katie Bell <katie@katharos.id.au>
2022-04-16 19:47:40 +1000

    Handle paste, emoji and large input in console. (#67)
    * Handle paste, emoji and large input in console.
    
    * Fix up replay of input on startup when there is no input to replay.

--- packages/runtime-python/runtime/webworker.js
+++ packages/runtime-python/runtime/webworker.js
@@ -15,6 +15,16 @@ const stdout = {
   flush: () => {},
 }
 
+const stderr = {
+  write: (s) => {
+    postMessage({
+      type: 'stderr',
+      stdout: s,
+    })
+  },
+  flush: () => {},
+}
+
 const stdin = {
   readline: () => {
     if (rerun) {
@@ -28,25 +38,29 @@ const stdin = {
       })
       return val
     }
-    // Send message to activate input mode
-    postMessage({
-      type: 'inputMode',
-    })
+
     let text = ''
-    Atomics.wait(stdinbuffer, 0, -1)
-    const numberOfElements = stdinbuffer[0]
-    stdinbuffer[0] = -1
-    const newStdinData = new Uint8Array(numberOfElements)
-    for (let i = 0; i < numberOfElements; i++) {
-      newStdinData[i] = stdinbuffer[1 + i]
+    // Keep reading from the buffer until we have a newline
+    while (text[text.length - 1] !== '\n') {
+      // Send message to activate input mode
+      postMessage({
+        type: 'stdin',
+      })
+      Atomics.wait(stdinbuffer, 0, -1)
+      const numberOfElements = stdinbuffer[0]
+      stdinbuffer[0] = -1
+      const newStdinData = new Uint8Array(numberOfElements)
+      for (let i = 0; i < numberOfElements; i++) {
+        newStdinData[i] = stdinbuffer[1 + i]
+      }
+      responseStdin = new TextDecoder('utf-8').decode(newStdinData)
+      text += responseStdin
+      postMessage({
+        type: 'inputValue',
+        value: text,
+      })
     }
-    responseStdin = new TextDecoder('utf-8').decode(newStdinData)
-    text += responseStdin
-    postMessage({
-      type: 'inputValue',
-      value: text,
-    })
-    return text //.replace('\r', '\n');
+    return text
   },
 }
 
@@ -58,7 +72,7 @@ const run = async () => {
     await pyodide.runPython(executorCode)
   } catch (err) {
     postMessage({
-      type: 'stdout',
+      type: 'stderr',
       stdout: err.toString(),
     })
   }
@@ -76,7 +90,7 @@ const initialise = async () => {
   pyodide = await loadPyodide({ fullStdLib: false, indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/' })
   pyodide.registerJsModule('fakeprint', {
     stdout: stdout,
-    stderr: stdout,
+    stderr: stderr,
     stdin: stdin,
   })
   pyodide.registerJsModule('nodetree', {
@@ -116,7 +130,7 @@ onmessage = function (e) {
       break
     case 'rerun':
       nodetree = e.data.nodetree
-      stdinbuffer = new Int32Array(e.data.buffer)
+      stdinbuffer = null
       readlines = e.data.readlines
       rerun = true
       run()
---
+++ packages/runtime-python/runtime/worker-manager.ts
@@ -0,0 +1,126 @@
+const INPUT_BUF_SIZE = 100
+
+export interface StandardIO {
+  stdin: () => Promise<string>
+  stdout: (s: string) => void
+  stderr: (s: string) => void
+}
+
+export enum WorkerState {
+  DISABLED = 0,
+  READY,
+  RUNNING,
+}
+
+export class WorkerManager {
+  private workerURL: string
+  private worker: Worker
+  private standardIO: StandardIO
+  private stdinbuffer: SharedArrayBuffer
+  private stdinbufferInt: Int32Array
+  private leftoverInput: string
+  private inputPlayback: string[]
+  private stateCallBack: (state: WorkerState) => void
+
+  constructor(workerURL: string, standardIO: StandardIO, stateCallback: (state: WorkerState) => void) {
+    this.workerURL = workerURL
+    this.worker = null
+    this.standardIO = standardIO
+    this.inputPlayback = []
+    this.stateCallBack = stateCallback
+
+    this.initialiseWorker()
+  }
+
+  initialiseWorker() {
+    if (!this.worker) {
+      this.worker = new Worker(this.workerURL)
+      this.worker.addEventListener('message', this.handleMessageFromWorker)
+    }
+  }
+
+  run(nodeTree: any) {
+    this.inputPlayback = []
+    this.stdinbuffer = new SharedArrayBuffer(INPUT_BUF_SIZE * Int32Array.BYTES_PER_ELEMENT)
+    this.stdinbufferInt = new Int32Array(this.stdinbuffer)
+    this.stdinbufferInt[0] = -1
+    this.worker.postMessage({
+      type: 'run',
+      nodetree: nodeTree,
+      buffer: this.stdinbuffer,
+    })
+  }
+
+  rerun(nodeTree: any) {
+    this.worker.postMessage({
+      type: 'rerun',
+      nodetree: nodeTree,
+      readlines: this.inputPlayback,
+    })
+  }
+
+  async provideStdin() {
+    let inputValue = this.leftoverInput
+    if (!inputValue) {
+      try {
+        inputValue = await this.standardIO.stdin()
+      } catch {
+        // TODO: In future we can send a signal to the worker to
+        // exit cleanly rather than killing the worker.
+        this.stop()
+      }
+    }
+
+    let data = new TextEncoder().encode(inputValue)
+    if (data.length > INPUT_BUF_SIZE - 1) {
+      const inputArray = Array.from(inputValue)
+      const textThatFits = new TextDecoder('utf-8').decode(data.slice(0, INPUT_BUF_SIZE - 1))
+      const chars = Math.max(1, Array.from(textThatFits).length - 2)
+      data = new TextEncoder().encode(inputArray.slice(0, chars).join(''))
+      this.leftoverInput = inputArray.slice(chars).join('')
+    } else {
+      this.leftoverInput = ''
+    }
+
+    if (this.stdinbuffer && this.stdinbufferInt) {
+      let startingIndex = 1
+      if (this.stdinbufferInt[0] > 0) {
+        startingIndex = this.stdinbufferInt[0]
+      }
+      data.forEach((value, index) => {
+        this.stdinbufferInt[startingIndex + index] = value
+      })
+
+      this.stdinbufferInt[0] = startingIndex + data.length - 1
+      Atomics.notify(this.stdinbufferInt, 0, 1)
+    }
+  }
+
+  stop() {
+    this.standardIO.stderr('\r\nProgram Stopped.\r\n')
+    this.stateCallBack(WorkerState.DISABLED)
+    this.worker.removeEventListener('message', this.handleMessageFromWorker)
+    this.worker.terminate()
+    this.worker = null
+    this.initialiseWorker()
+  }
+
+  handleMessageFromWorker = (event) => {
+    const type = event.data.type
+    if (type === 'ready') {
+      this.stateCallBack(WorkerState.READY)
+    } else if (type === 'stdout') {
+      this.standardIO.stdout(event.data.stdout)
+    } else if (type === 'stderr') {
+      this.standardIO.stderr(event.data.stderr)
+    } else if (type === 'stdin') {
+      this.provideStdin()
+    } else if (type === 'inputValue') {
+      this.inputPlayback.push(event.data.value)
+    } else if (type === 'runtime_capture') {
+      parent.postMessage(event.data, process.env.EDITOR_DOMAIN)
+    } else if (type === 'finished') {
+      this.stateCallBack(WorkerState.READY)
+    }
+  }
+}
