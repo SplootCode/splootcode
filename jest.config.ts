b43d0413a2fe1c334446f41ae6054abb86ed749b
Katie Bell <katie@katharos.id.au>
2022-06-28 16:18:22 +1000

    Add test for rendering a string literal node.

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
   testPathIgnorePatterns: ['node_modules', 'lib', 'dist'],
   moduleNameMapper: {
     '@splootcode/(.*)': '<rootDir>/packages/$1',
+    '\\.(css|less)$': 'identity-obj-proxy',
   },
 }
 export default config
---
+++ packages/editor/components/node_block.test.tsx
@@ -0,0 +1,57 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock'
+import React from 'react'
+import pretty from 'pretty'
+import { act } from 'react-dom/test-utils'
+import { render, unmountComponentAtNode } from 'react-dom'
+
+import { PythonStringLiteral } from '@splootcode/language-python/nodes/literals'
+import { loadTypes } from '@splootcode/language-python/type_loader'
+
+import { EditorNodeBlock } from './node_block'
+import { NodeBlock } from '../layout/rendered_node'
+import { NodeSelectionState } from '../context/selection'
+
+let container: HTMLElement = null
+
+beforeAll(() => {
+  loadTypes()
+})
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+it('renders a single string literal node', () => {
+  const node = new PythonStringLiteral(null, 'hello this is string')
+  const nodeBlock = new NodeBlock(null, node, null, 0)
+  nodeBlock.calculateDimensions(0, 0, null)
+
+  act(() => {
+    render(
+      <svg height={nodeBlock.rowHeight + nodeBlock.indentedBlockHeight} width={nodeBlock.width}>
+        <EditorNodeBlock block={nodeBlock} selectionState={NodeSelectionState.UNSELECTED} />
+      </svg>,
+      container
+    )
+  })
+  expect(container.children).toHaveLength(1)
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+    "<svg height=\\"20\\" width=\\"30\\">
+      <g>
+        <path class=\\"svgsplootnode\\" d=\\"M 4 0 q -4,0 -4,4 v 12 q 0,4 4,4 h 22 q 4,0 4,-4 v -12 q 0,-4 -4,-4 z\\"></path><text class=\\"string-literal \\" x=\\"4\\" y=\\"15\\" style=\\"fill: var(--code-green-200);\\" xml:space=\\"preserve\\">\\"hello this is string\\"</text>
+      </g>
+    </svg>"
+  `)
+})
