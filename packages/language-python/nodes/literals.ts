612ccdbe58fd251d4d2d77cea0f33bd6b92e66d4
Katie Bell <katie@katharos.id.au>
2022-06-29 12:45:13 +1000

    Move Python strings to separate file

--- packages/language-python/nodes/literals.ts
+++ packages/language-python/nodes/literals.ts
@@ -1,12 +1,4 @@
-import {
-  ConstantNode,
-  KeywordType,
-  ParseNode,
-  ParseNodeType,
-  StringNode,
-  StringTokenFlags,
-  TokenType,
-} from 'structured-pyright'
+import { ConstantNode, KeywordType, ParseNode, ParseNodeType } from 'structured-pyright'
 
 import { HighlightColorCategory } from '@splootcode/core/colors'
 import {
@@ -20,7 +12,6 @@ import {
 import {
   NodeCategory,
   SuggestionGenerator,
-  getAutocompleRegistry,
   registerAutocompleter,
   registerNodeCateogry,
 } from '@splootcode/core/language/node_category_registry'
@@ -32,91 +23,7 @@ import { SuggestedNode } from '@splootcode/core/language/autocomplete/suggested_
 
 export const PYTHON_NONE = 'PYTHON_NONE'
 export const PYTHON_BOOL = 'PYTHON_BOOL'
-export const PYTHON_STRING = 'STRING_LITERAL'
 export const PYTHON_NUMBER_LITERAL = 'NUMERIC_LITERAL'
-
-class StringGenerator implements SuggestionGenerator {
-  staticSuggestions(parent: ParentReference, index: number) {
-    const emptyString = new PythonStringLiteral(null, '')
-    const suggestedNode = new SuggestedNode(emptyString, 'empty string', 'string text empty', true, 'empty string')
-    return [suggestedNode]
-  }
-
-  dynamicSuggestions(parent: ParentReference, index: number, textInput: string) {
-    if (textInput.startsWith("'") || textInput.startsWith('"')) {
-      let value = textInput.slice(1)
-      if (value.length !== 0 && value[value.length - 1] === textInput[0]) {
-        value = value.slice(0, value.length - 1)
-      }
-      const customString = new PythonStringLiteral(null, value)
-      const suggestedNode = new SuggestedNode(customString, `string ${value}`, textInput, true, 'string')
-      return [suggestedNode]
-    }
-    return []
-  }
-}
-
-export class PythonStringLiteral extends PythonNode {
-  constructor(parentReference: ParentReference, value: string) {
-    super(parentReference, PYTHON_STRING)
-    this.properties = { value: value }
-  }
-
-  getValue() {
-    return this.properties.value
-  }
-
-  getEditableProperty() {
-    return 'value'
-  }
-
-  generateParseTree(parseMapper: ParseMapper): StringNode {
-    const val = this.getValue()
-    return {
-      nodeType: ParseNodeType.String,
-      id: parseMapper.getNextId(),
-      length: 0,
-      start: 0,
-      value: val,
-      hasUnescapeErrors: false,
-      token: {
-        escapedValue: val,
-        start: 0,
-        length: 0,
-        flags: StringTokenFlags.None,
-        type: TokenType.String,
-        prefixLength: 0,
-        quoteMarkLength: 0,
-      },
-    }
-  }
-
-  static deserializer(serializedNode: SerializedNode): PythonStringLiteral {
-    return new PythonStringLiteral(null, serializedNode.properties.value)
-  }
-
-  static register() {
-    const stringLiteral = new TypeRegistration()
-    stringLiteral.typeName = PYTHON_STRING
-    stringLiteral.deserializer = PythonStringLiteral.deserializer
-    stringLiteral.properties = ['value']
-    stringLiteral.layout = new NodeLayout(HighlightColorCategory.LITERAL_STRING, [
-      new LayoutComponent(LayoutComponentType.STRING_LITERAL, 'value'),
-    ])
-    stringLiteral.pasteAdapters[PYTHON_EXPRESSION] = (node: SplootNode) => {
-      const exp = new PythonExpression(null)
-      exp.getTokenSet().addChild(node)
-      return exp
-    }
-    registerType(stringLiteral)
-    registerNodeCateogry(PYTHON_STRING, NodeCategory.PythonExpressionToken)
-    registerAutocompleter(NodeCategory.PythonExpressionToken, new StringGenerator())
-
-    const registry = getAutocompleRegistry()
-    registry.registerPrefixOverride('"', NodeCategory.PythonExpressionToken, new StringGenerator())
-    registry.registerPrefixOverride("'", NodeCategory.PythonExpressionToken, new StringGenerator())
-  }
-}
 
 class NumberGenerator implements SuggestionGenerator {
   dynamicSuggestions(parent: ParentReference, index: number, textInput: string) {
