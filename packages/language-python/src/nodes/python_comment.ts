646ae48e0c901c138858648d10b77a40c92e6000
ikramsaedi <ikramsaedi@gmail.com>
2022-12-28 02:59:07 +1100

    change comments to use statements instead of expressions

--- packages/language-python/src/nodes/python_comment.ts
+++ packages/language-python/src/nodes/python_comment.ts
@@ -1,10 +1,7 @@
-import { ParseNodeType, StringNode, StringTokenFlags, TokenType } from 'structured-pyright'
-
 import {
   HighlightColorCategory,
   LayoutComponent,
   LayoutComponentType,
-  NodeBoxType,
   NodeCategory,
   NodeLayout,
   ParentReference,
@@ -18,14 +15,14 @@ import {
   registerNodeCateogry,
   registerType,
 } from '@splootcode/core'
-import { PYTHON_EXPRESSION, PythonExpression } from './python_expression'
+import { PYTHON_STATEMENT, PythonStatement } from './python_statement'
 import { ParseMapper } from '../analyzer/python_analyzer'
 import { PythonNode } from './python_node'
 
 export const PYTHON_COMMENT = 'PY_COMMENT'
 
 class CommentGenerator implements SuggestionGenerator {
-    // like the name implies, these are autocorrect suggestions that always remain the same 
+  // like the name implies, these are autocorrect suggestions that always remain the same
   staticSuggestions(parent: ParentReference, index: number) {
     console.log('static suggestions!')
     const emptyComment = new PythonComment(null, '')
@@ -33,7 +30,7 @@ class CommentGenerator implements SuggestionGenerator {
     const suggestedNode = new SuggestedNode(emptyComment, 'empty comment', 'comment', true, 'comment')
     return [suggestedNode]
   }
-    // while these ones change as you type 
+  // while these ones change as you type
   dynamicSuggestions(parent: ParentReference, index: number, textInput: string) {
     console.log('dynamicSuggestions!!')
     if (textInput.startsWith("'") || textInput.startsWith('"')) {
@@ -63,54 +60,37 @@ export class PythonComment extends PythonNode {
     return 'value'
   }
 
-//   helps the code be parsed as Code :sweat:
-  generateParseTree(parseMapper: ParseMapper): StringNode {
-    const val = this.getValue()
-    return {
-      nodeType: ParseNodeType.String,
-      id: parseMapper.getNextId(),
-      length: 0,
-      start: 0,
-      value: val,
-      hasUnescapeErrors: false,
-      token: {
-        escapedValue: val,
-        start: 0,
-        length: 0,
-        flags: StringTokenFlags.None,
-        type: TokenType.String,
-        prefixLength: 0,
-        quoteMarkLength: 0,
-      },
-    }
+  //  No reason to create a parse tree for comments
+  generateParseTree(parseMapper: ParseMapper): any {
+    return null
   }
 
-//   turns the comment's serialized JSON into a Python Comment node
+  //   turns the comment's serialized JSON into a Python Comment node
   static deserializer(serializedNode: SerializedNode): PythonComment {
     return new PythonComment(null, serializedNode.properties.value)
   }
 
-// this is what the Type Loader uses to register this
+  // this is what the Type Loader uses to register this
   static register() {
     console.log('register is being called')
-    const comment = new TypeRegistration()
-    comment.typeName = PYTHON_COMMENT
-    comment.deserializer = PythonComment.deserializer
-    comment.properties = ['value']
-    comment.layout = new NodeLayout(HighlightColorCategory.VARIABLE, [
+    const typeRegistration = new TypeRegistration()
+    typeRegistration.typeName = PYTHON_COMMENT
+    typeRegistration.deserializer = PythonComment.deserializer
+    typeRegistration.properties = ['value']
+    typeRegistration.layout = new NodeLayout(HighlightColorCategory.VARIABLE, [
       new LayoutComponent(LayoutComponentType.CAP, '#'),
       new LayoutComponent(LayoutComponentType.PROPERTY, 'value'),
     ])
-    comment.pasteAdapters[PYTHON_EXPRESSION] = (node: SplootNode) => {
-      const exp = new PythonExpression(null)
-      exp.getTokenSet().addChild(node)
-      return exp
+    typeRegistration.pasteAdapters[PYTHON_STATEMENT] = (node: SplootNode) => {
+      const statement = new PythonStatement(null)
+      statement.getStatement().addChild(node)
+      return statement
     }
-    registerType(comment)
-    registerNodeCateogry(PYTHON_COMMENT, NodeCategory.PythonExpressionToken)
-    registerAutocompleter(NodeCategory.PythonExpressionToken, new CommentGenerator())
+    registerType(typeRegistration)
+    registerNodeCateogry(PYTHON_COMMENT, NodeCategory.PythonStatementContents)
+    registerAutocompleter(NodeCategory.PythonStatementContents, new CommentGenerator())
 
     const registry = getAutocompleteRegistry()
-    registry.registerPrefixOverride('#', NodeCategory.PythonExpressionToken, new CommentGenerator())
+    registry.registerPrefixOverride('#', NodeCategory.PythonStatementContents, new CommentGenerator())
   }
 }
