c1317bb2498e2f704e69b51d67d3abab3d7a0159
Harrison Shoebridge <harrison@theshoebridges.com>
2023-02-14 14:50:56 +1100

    Disable 'Create project' button if projectID is empty

--- packages/components/src/save_project_modal.tsx
+++ packages/components/src/save_project_modal.tsx
@@ -40,7 +40,7 @@ export function SaveProjectModal(props: SaveProjectModalProps) {
   const [validID, setValidID] = useState(true)
 
   useEffect(() => {
-    if (clonedFrom) {
+    if (clonedFrom && projectLoader) {
       const generate = async () => {
         const [name, title] = await projectLoader.generateValidProjectId(newOwner, clonedFrom.name, clonedFrom.title)
         setProjectID(name)
@@ -51,7 +51,7 @@ export function SaveProjectModal(props: SaveProjectModalProps) {
       setProjectID('')
       setProjectTitle('')
     }
-  }, [clonedFrom])
+  }, [clonedFrom, projectLoader])
 
   useEffect(() => {
     if (projectID === '') {
@@ -106,7 +106,7 @@ export function SaveProjectModal(props: SaveProjectModalProps) {
               </FormControl>
             </ModalBody>
             <ModalFooter>
-              <Button colorScheme="blue" disabled={projectTitle === '' || !validID} type="submit">
+              <Button colorScheme="blue" disabled={projectTitle === '' || projectID === '' || !validID} type="submit">
                 Create project
               </Button>
             </ModalFooter>
