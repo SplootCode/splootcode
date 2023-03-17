998e7ea8db83ef6742197013533f0583a183fcf8
Katie Bell <katie@katharos.id.au>
2023-03-17 17:33:50 +1100

    Remove si icons.

--- packages/components/src/run_type_icon.tsx
+++ packages/components/src/run_type_icon.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { AiOutlineClockCircle } from 'react-icons/ai'
+import { BiGlobe } from 'react-icons/bi'
 import { FaTerminal } from 'react-icons/fa'
 import { Icon } from '@chakra-ui/react'
 import { RunType } from '@splootcode/core'
-import { SiAiohttp } from 'react-icons/si'
 
 export function RunTypeIcon(props: { runType: RunType }) {
   switch (props.runType) {
     case RunType.COMMAND_LINE:
       return <Icon as={FaTerminal} boxSize={6} backgroundColor="pink.700" p={1} borderRadius={3} />
     case RunType.HTTP_REQUEST:
-      return <Icon as={SiAiohttp} boxSize={6} backgroundColor="blue.700" p={1} borderRadius={3} />
+      return <Icon as={BiGlobe} boxSize={6} backgroundColor="blue.700" p={1} borderRadius={3} />
     case RunType.SCHEDULE:
       return <Icon as={AiOutlineClockCircle} boxSize={6} backgroundColor="teal.700" p={1} borderRadius={3} />
     default:
