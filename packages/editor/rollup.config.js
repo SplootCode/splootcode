e554ae670952d5d81b1938485436837193f71c91
Katie Bell <katie@katharos.id.au>
2023-03-30 19:06:23 +1100

    Remove node polyfills since they're handled within structured-pyright now.

--- packages/editor/rollup.config.js
+++ packages/editor/rollup.config.js
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs'
 import css from 'rollup-plugin-import-css'
 import dts from 'rollup-plugin-dts'
 import typescript from '@rollup/plugin-typescript'
@@ -6,7 +7,14 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 export default [
   {
     external: ['react', '@emotion/core', '@emotion/styled', '@chakra-ui/react', '@chakra-ui/icons'],
-    plugins: [typescript(), css(), nodeResolve({ resolveOnly: ['react-icons', 'http-status-codes'] })],
+    plugins: [
+      typescript(),
+      commonjs(),
+      nodeResolve({
+        resolveOnly: ['react-icons', 'http-status-codes', 'content-type', 'xterm', 'structured-pyright'],
+      }),
+      css({ minify: true }),
+    ],
     input: 'src/index.ts',
     output: [
       {
