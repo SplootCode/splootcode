ac5fe72942a31c01de9aa2e07c78fd53f7585f4a
Harrison Shoebridge <harrison@theshoebridges.com>
2023-05-02 13:31:46 +1000

    Expose some more UI components for Flask requests/responses

--- packages/editor/src/runtime/response_viewer.tsx
+++ packages/editor/src/runtime/response_viewer.tsx
@@ -29,32 +29,64 @@ function TextBased(props: TextBasedProps) {
   )
 }
 
-export interface ResponseViewerProps {
-  response: HTTPResponse
+export interface HeadersProps {
+  headers: Record<string, string>
 }
 
-export function ResponseViewer(props: ResponseViewerProps) {
-  const { response } = props
-
-  let body = null
-
-  let headers = []
-
-  if (response) {
-    const contentType = parseContentType(response.headers['Content-Type'])
-
-    if (contentType.type === 'application/json') {
-      body = <TextBased content={JSON.stringify(JSON.parse(response.body), null, 2)}></TextBased>
-    } else {
-      body = <TextBased content={response.body} />
-    }
-
-    headers = Object.entries(response.headers)
-  } else {
-    body = <Text>No content...</Text>
-  }
-
+export function Headers(props: HeadersProps) {
+  const { headers } = props
   const { isOpen, onToggle } = useDisclosure()
+
+  const headersList = Object.entries(headers)
+
+  return (
+    <Box>
+      {' '}
+      <Flex alignItems={'center'}>
+        <Text as="h2" fontWeight={'bold'} mb="1">
+          Headers
+        </Text>
+        <IconButton
+          aria-label="Expand headers"
+          icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          onClick={onToggle}
+          variant="ghost"
+          size={'xs'}
+          fontSize="md"
+        >
+          {' '}
+        </IconButton>
+      </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        {headersList.length == 0 && <Text fontStyle={'italic'}>No headers...</Text>}
+
+        {headersList.map(([key, value], i) => {
+          return (
+            <Box mb="2" key={i}>
+              <Input
+                px="1"
+                size={'sm'}
+                variant="filled"
+                readOnly
+                value={key}
+                mb="1"
+                backgroundColor={'gray.800'}
+              ></Input>
+              <Input px="1" size={'sm'} variant="filled" readOnly value={value} backgroundColor={'gray.800'}></Input>
+            </Box>
+          )
+        })}
+      </Collapse>
+    </Box>
+  )
+}
+
+export interface StatusCodeInfoProps {
+  statusCode: number
+}
+
+export function StatusCodeInfo(props: StatusCodeInfoProps) {
+  const { statusCode } = props
 
   const statusColour = (code) => {
     if (code >= 200 && code < 300) {
@@ -66,87 +98,87 @@ export function ResponseViewer(props: ResponseViewerProps) {
     return 'white'
   }
 
-  const { onCopy } = useClipboard(response?.body)
+  return (
+    <Text fontFamily={'Inconsolata'}>
+      {statusCode ? (
+        <>
+          <Text as="span" color={statusColour(statusCode)}>
+            {statusCode}
+          </Text>
+          {' | '}
+          {getReasonPhrase(statusCode)}
+        </>
+      ) : (
+        <Text as="span" color={'gray.400'}>
+          Loading...
+        </Text>
+      )}
+    </Text>
+  )
+}
 
+export function BodyInfo(props: { body: string; rawContentType: string }) {
+  const { body, rawContentType } = props
+  let formattedBody = null
+
+  if (body) {
+    const contentType = parseContentType(rawContentType)
+
+    if (contentType.type === 'application/json') {
+      formattedBody = <TextBased content={JSON.stringify(JSON.parse(body), null, 2)}></TextBased>
+    } else {
+      formattedBody = <TextBased content={body} />
+    }
+  } else {
+    formattedBody = <Text fontStyle={'italic'}>No content...</Text>
+  }
+
+  const { onCopy } = useClipboard(body)
+  return (
+    <Box>
+      <HStack justifyContent={'space-between'} mb="1">
+        <Text as="h2" fontWeight={'bold'}>
+          Body
+        </Text>
+
+        <IconButton aria-label="Copy body" icon={<CopyIcon />} size="sm" onClick={onCopy}></IconButton>
+      </HStack>
+
+      {formattedBody}
+    </Box>
+  )
+}
+
+export function ResponseViewerInfo(props: ResponseViewerProps) {
+  const { response } = props
+
+  return (
+    <>
+      <Box mb="4">
+        <Text as="h2" fontWeight={'bold'} mb="1">
+          Status code
+        </Text>
+        <StatusCodeInfo statusCode={response?.statusCode} />
+      </Box>
+
+      <Box mb="4">
+        <Headers headers={response?.headers || {}} />
+      </Box>
+
+      <BodyInfo body={response?.body} rawContentType={response?.headers['Content-Type']} />
+    </>
+  )
+}
+
+export interface ResponseViewerProps {
+  response: HTTPResponse
+}
+
+export function ResponseViewer(props: ResponseViewerProps) {
   return (
     <Box height={'100%'} backgroundColor={'#040810'}>
       <Box p="3" overflowY={'auto'} height={'100%'} id="response-viewer">
-        <Box mb="4">
-          <Text as="h2" fontWeight={'bold'} mb="1">
-            Status code
-          </Text>
-          <Text fontFamily={'Inconsolata'}>
-            {response ? (
-              <>
-                <Text as="span" color={statusColour(response.statusCode)}>
-                  {response.statusCode}
-                </Text>
-                {' | '}
-                {getReasonPhrase(response?.statusCode)}
-              </>
-            ) : (
-              <Text as="span" color={'gray.400'}>
-                Loading...
-              </Text>
-            )}
-          </Text>
-        </Box>
-
-        <Box mb="4">
-          <Flex alignItems={'center'}>
-            <Text as="h2" fontWeight={'bold'} mb="1">
-              Headers
-            </Text>
-            <IconButton
-              aria-label="Expand headers"
-              icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-              onClick={onToggle}
-              variant="ghost"
-              size={'xs'}
-              fontSize="md"
-            >
-              {' '}
-            </IconButton>
-          </Flex>
-
-          <Collapse in={isOpen} animateOpacity>
-            {headers.map(([key, value], i) => {
-              return (
-                <Box mb="2" key={i}>
-                  <Input
-                    px="1"
-                    size={'sm'}
-                    variant="filled"
-                    readOnly
-                    value={key}
-                    mb="1"
-                    backgroundColor={'gray.800'}
-                  ></Input>
-                  <Input
-                    px="1"
-                    size={'sm'}
-                    variant="filled"
-                    readOnly
-                    value={value}
-                    backgroundColor={'gray.800'}
-                  ></Input>
-                </Box>
-              )
-            })}
-          </Collapse>
-        </Box>
-
-        <Box>
-          <HStack justifyContent={'space-between'} mb="1">
-            <Text as="h2" fontWeight={'bold'}>
-              Body
-            </Text>
-
-            <IconButton aria-label="Copy body" icon={<CopyIcon />} size="sm" onClick={onCopy}></IconButton>
-          </HStack>
-
-          {body}
-        </Box>
+        <ResponseViewerInfo response={props.response}></ResponseViewerInfo>
       </Box>
     </Box>
   )
